2852f3f2d687b639e38bac93204c9b0022d984a5
Sandro <sandro.jaeckel@gmail.com>
2020-08-08 09:09:57 +0200

    Fix wttr.in sometimes empty (#95)
    * Remove not needed quotes
    
    This was done using the simplify option of shfmt
    
    * Remove whitespace
    
    also done by shfmt
    
    * Check for empty weather data before writting
    
    If you have no internet connection and the wttr.in segment gets refreshed
    it will be an empty segment (only the arrow) until it gets refreshed again.
    
    This checks if the response by wttr.in is empty and does not attempt
    to write whitespace to the cache.

--- src/segments/wttr.bash
+++ src/segments/wttr.bash
@@ -5,30 +5,32 @@ format=${SETTINGS_WTTR_FORMAT:-'%p;%t;%w'}
 refresh_rate="${SEGMENTS_WTTR_REFRESH_RATE:-600}"
 
 segments::wttr_refresh() {
-  if [[ ! -f "$SEGMENT_CACHE" ]]; then
+  if [[ ! -f $SEGMENT_CACHE ]]; then
     debug::log "No cache folder"
   fi
 
-  if [[ -f "$SEGMENT_CACHE" ]]; then
+  if [[ -f $SEGMENT_CACHE ]]; then
     last_update=$(stat -f "%m" "$SEGMENT_CACHE")
   else
     last_update=0
   fi
 
   current_time=$(date +%s)
-  time_since_update=$(( current_time - last_update ))
+  time_since_update=$((current_time - last_update))
 
-  if [[ "$time_since_update" -lt "$refresh_rate" ]]; then
+  if [[ $time_since_update -lt $refresh_rate ]]; then
     return 0
   fi
 
-  curl -H "Accept-Language: ${LANG%_*}" --compressed "wttr.in/${location}?format=${format}" | tr -d '\n' | tr ';' '\n' > "$SEGMENT_CACHE"
+  weather_data="$(curl -H "Accept-Language: ${LANG%_*}" --compressed "wttr.in/${location}?format=${format}" | tr -d '\n' | tr ';' '\n')"
+  if [[ -n $weather_data ]]; then
+    echo "$weather_data" >"$SEGMENT_CACHE"
+  fi
 }
 
-
 segments::wttr() {
-  if [[ -f "$SEGMENT_CACHE" ]]; then
-    mapfile -t result < "$SEGMENT_CACHE"
+  if [[ -f $SEGMENT_CACHE ]]; then
+    mapfile -t result <"$SEGMENT_CACHE"
     print_themed_segment 'normal' "${result[@]}"
   fi
   execute::execute_nohup_function segments::wttr_refresh
